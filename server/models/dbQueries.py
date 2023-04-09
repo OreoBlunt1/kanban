17671a272e9c96744663607c05d86941f97ac84d
diplodocx <zabarinternational@gmail.com>
2023-04-09 10:43:58 +0300

    ORM created

--- server/models/dbQueries.py
+++ server/models/dbQueries.py
@@ -1,5 +1,10 @@
-from .dbModel import User, db
+from .dbModel import User, Lobby, LobbyUser, Task,  db
 def test_insert():
-    db.session.add(
-        User(Login="harry", Password="поттер", Photo_Src="/src/img/harry", Email="harry@mail.ru"))
-    db.session.commit()
+    pass
+    # db.session.add(
+    #     Task(Task_Tittle="отомстить Итачи", Task_Inittime="1990-12-12 13:00:00", Task_Deadline="2020-12-12 13:00:00",
+    #          Task_Status="doing", Lobby_ID=2)
+    # )
+    # res = db.session.query(Task.Task_ID, Task.Task_Tittle, Task.Lobby_ID).all()
+    # print(res[0].Lobby_ID)
+    # db.session.commit()
