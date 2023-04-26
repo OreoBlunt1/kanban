5bf44afe6defda5e298c33da115f65ab628c3103
diplodocx <zabarinternational@gmail.com>
2023-04-26 14:35:36 +0300

    minor docs changes

--- FastAPIServer/app.py
+++ FastAPIServer/app.py
@@ -7,26 +7,17 @@ from src.auth.routers import jwt
 from src.auth.schemas import UserRead, UserCreate
 from src.kanban.routers import kanban
 
-app = FastAPI(title="kanban App")
+app = FastAPI(title="kanban app")
 
 current_user = fastapi_users.current_user()
 
-
-@app.get("/test")
-def protected_route(user: User = Depends(current_user)):
-    return f"Hello, {user.email}"
-
-
-
 app.include_router(kanban)
 app.include_router(jwt)
-
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
     prefix="/auth",
     tags=["auth"],
 )
-
 app.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix="/auth",
