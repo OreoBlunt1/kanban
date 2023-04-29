9abf15284a942faedacd566d3176b714b8372f5b
diplodocx <zabarinternational@gmail.com>
2023-04-29 23:11:28 +0300

    change db to postgresql

--- FastAPIServer/src/auth/utils.py
+++ FastAPIServer/src/auth/utils.py
@@ -3,17 +3,7 @@ from .models import User
 from fastapi import Depends
 from fastapi_users.db import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-
-DATABASE_URL = "sqlite+aiosqlite:///kanban.db"
-
-
-engine = create_async_engine(DATABASE_URL)
-async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
-
-
-async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with async_session_maker() as session:
-        yield session
+from database import get_async_session
 
 
 async def get_user_db(session: AsyncSession = Depends(get_async_session)):
