6bb4237f4e3a679e5fdf2403becd41d528cff948
OreoBlunt1 <dima_zhdanov_02@mail.ru>
2023-06-01 13:46:56 +0300

    final

--- client/src/components/Board/Board.js
+++ client/src/components/Board/Board.js
@@ -1,15 +1,24 @@
-import React from "react";
-import classes from "./Board.module.css";
-import BoardColumn from "./BoardColumn/BoardColumn";
+import React from 'react';
+import classes from './Board.module.css';
+import BoardColumn from './BoardColumn/BoardColumn';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
+export function moveTask(columnId, taskId) {
+	console.log('moved column #', columnId);
+	console.log('moved task # ', taskId);
+}
 
 function Board() {
 	return (
-		<div className={classes.Board}>
-			<BoardColumn type={"В планах"} />
-			<BoardColumn type={"В работе"} />
-			<BoardColumn type={"Отложено"} />
-			<BoardColumn type={"Готово"} />
-		</div>
+		<DndProvider backend={HTML5Backend}>
+			<div className={classes.Board}>
+				<BoardColumn type={'В планах'} id={'1'} />
+				<BoardColumn type={'В работе'} id={'2'} />
+				<BoardColumn type={'Отложено'} id={'3'} />
+				<BoardColumn type={'Готово'} id={'4'} />
+			</div>
+		</DndProvider>
 	);
 }
 
